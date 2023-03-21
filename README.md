b0a97efc61146259ddde8246263b16292ceb5173
KhangPham9 <khangpham9999@csu.fullerton.edu>
2023-03-21 01:26:20 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# RosTurleSim
+
+Some code to move the turtle in the turtlesim package from ros.
+
+# How to start test the code
+
+ - Run the turtlesim node package
+   - rosrun turtlesim turtlesim\_node
+
+ - Run the turtle\_cmd\_vel node
+   - rosrun turtle turtle\_cmd\_vel
+
+# How to build the package
+ 
+ - Download this repo in the src folder of catkin\_ws
+ - Then, do catkin\_make in the catkin\_ws folder 
+ - Run the command "source devel/setup.bash"
+
---
+++ src/turtle_move.cpp
@@ -0,0 +1,128 @@
+
+#include <ros/ros.h>
+#include <turtlesim/Pose.h>
+#include <geometry_msgs/Twist.h>
+#include <cmath>
+
+class Turtle {
+    private:
+        ros::Publisher cmd_vel_pub;
+        ros::Subscriber pose_sub;
+
+        float x, y, yaw;
+    public:
+        Turtle(ros::NodeHandle *n) {
+            cmd_vel_pub = n->advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 100);
+            pose_sub = n->subscribe("/turtle1/pose", 100, &Turtle::pose_callback, this);
+        }
+
+        void pose_callback(const turtlesim::Pose::ConstPtr& pose) {
+            // ROS_INFO("\nTurtle x: %f\nTurtle y: %f\nTurtle theta: %f\nTurtle linear_velocity: %f\nTurtle angular_velocity: %f\n",
+            //         pose->x, pose->y, pose->theta, pose->linear_velocity, pose->angular_velocity);
+                    // geometry_msgs::Twist t;
+    
+                    x = pose->x;
+                    y = pose->y;
+
+                    yaw = pose->theta;
+                    // ROS_INFO("Yaw: %f", yaw);
+                    // cmd_vel_pub.publish(t);
+        }
+
+        void move(float speed, float distance) {
+            float start_x = x,
+                  start_y = y;
+                //   start_theta = theta;
+
+            float distance_moved = 0.0;
+
+            ros::Rate move_frequency(10);
+
+            geometry_msgs::Twist t;
+            t.linear.x = speed;
+            t.linear.y = 0;
+            t.linear.z = 0;
+            t.angular.x = 0;
+            t.angular.y = 0;
+            t.angular.z = 0;
+
+            while(ros::ok()) {
+                cmd_vel_pub.publish(t);
+
+                distance_moved +=   0.1 * std::abs(std::sqrt(std::pow(x - start_x, 2) + std::pow(y - start_y , 2)));
+                move_frequency.sleep();
+
+                ROS_INFO("Turtle moved %f\n", distance_moved);
+
+                if (distance_moved >= distance) {
+                    ROS_INFO("Finished moving");
+                    break;
+                }
+
+                ros::spinOnce();
+            }
+
+        }
+
+
+        void rotate(float speed, float theta) {
+            float start_theta = yaw;
+
+            ros::Rate rotate_frequency(10);
+
+            geometry_msgs::Twist t;
+            t.linear.x = 0;
+            t.linear.y = 0;
+            t.linear.z = 0;
+            t.angular.x = 0;
+            t.angular.y = 0;
+            t.angular.z = speed;
+
+            float theta_rotated = 0.0;
+            while(ros::ok()) {
+                cmd_vel_pub.publish(t);
+
+                rotate_frequency.sleep();
+
+                theta_rotated += 0.1 * std::abs((start_theta - yaw));
+                // ROS_INFO("Yaw %f", yaw);
+                ROS_INFO("Turtle rotated %f\n", theta_rotated);
+
+                if (theta_rotated >= theta) {
+                    ROS_INFO("Finished Rotation");
+                    break;
+                }
+
+                ros::spinOnce();
+
+
+            }
+        }
+};
+
+
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "robot_move_topic");
+
+    ros::NodeHandle n;
+
+    Turtle t = Turtle(&n);
+
+    ros::Rate loop_rate(10);
+
+    // while (ros::ok()) {
+
+    //     ros::spinOnce();
+    //     loop_rate.sleep();
+    // }
+
+    t.move(1.0, 20.0);
+    t.rotate(0.1, 2);
+    t.move(1.0, 20.0);
+
+    
+    return 0;
+}
+
+
